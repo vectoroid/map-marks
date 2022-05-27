ec2489d07e434cb1240d395659d4ba431c350bb3
Tom Malone <tmalone+git@gmail.com>
2022-05-27 10:18:51 -0400

    Removed unused type "Numeric"

--- mapmarks/api/types.py
+++ mapmarks/api/types.py
@@ -5,8 +5,5 @@ import uuid
 GeolocationCategory = typing.Literal["Reefer", "Tobacco"]
 
 # GeoJSON Types
-GeojsonType = typing.Literal["Point", "Feature"] # not all GeoJSON types are used, presently
-
-# Utility Types
-Numeric = typing.Union[int, float]
 GeolocationId = typing.NewType('GeolocationId', uuid.UUID)
+GeojsonType = typing.Literal["Point", "Feature"] # not all GeoJSON types are used, presently
