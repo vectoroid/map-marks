f376f1865f828ab2302c3092167c1c339ead16bb
Tom Malone <tmalone+git@gmail.com>
2022-05-27 09:50:20 -0400

    Added Deta Base initialization

--- mapmarks/__init__.py
+++ mapmarks/__init__.py
@@ -5,7 +5,9 @@ MapMarkr app - save your place in life
 __version__ = "0.0.1"
 
 # import app modules
-from .config import AppSettings
-# from .schema import *
-from .types import Position
-from .types import Props
+from deta import Deta
+from mapmarks.api.config import AppSettings
+
+# initialize Deta Base
+deta = Deta()
+async_db = deta.Base(AppSettings.DB.name)
