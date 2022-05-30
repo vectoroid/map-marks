ac6f662c85c1551f3ccd621a051513b065379bb5
Tom Malone <tmalone+git@gmail.com>
2022-05-30 19:27:26 -0400

    Added aiohttp as dependency, because Deta Base() uses it to communicate with their HTTP/REST API (need aiohttp.ClientError exception class). Wrote async method for instantiating Deta.Base() instance. Wrote model intance methods for saving, updating and deleting model records from Deta.Base.

--- mapmarks/api/models/base.py
+++ mapmarks/api/models/base.py
@@ -4,16 +4,31 @@ MapMarkr :: I/O Schema
   I/O -- user input and app output, in the form of HTTPResponses, defined by 
   classes in FastAPI, or--more probably--Starlette.
 """
+import aiohttp
+import contextlib
 import typing
 import uuid
 
 from pydantic import Extra
-from pydantic import confloat
-from pydantic import validator
+from pydantic import Field
 from pydantic import BaseModel as PydanticBase
 
-from mapmarks.api.types import GeojsonType, GeolocationCategory, GeolocationId
-# from mapmarks.api.interface import TimestampMixin
+from deta import Deta
+deta = Deta()
+
+from mapmarks.api.config import AppSettings
+
+@contextlib.asynccontextmanager
+async def async_db_client(db_name: str):
+    db_client = deta.AsyncBase(db_name)
+    
+    try:
+        yield db_client
+    except aiohttp.ClientError as e:
+        print(e)
+    finally:
+        await db_client.close()
+        
 
 # Root subclass 
 # -  simplest method to apply universal config options to all models
@@ -26,6 +41,35 @@ class BaseModel(PydanticBase):
           my actual I/O models, so by renaming (effectively) pydantic.BaseModel to pydantic.PydanticBase, 
           I avoid having to replace all instances of 'BaseModel' throughout the app.
     """
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    version: int = 1
+    db_name: typing.ClassVar
+    
     class Config:
         anystr_strip_whitespace: bool = True    # always strip whitespace from user-input strings
         extra: str = Extra.forbid
+        
+    def dict(self, *args, **kwargs) -> dict:
+        """
+        Inject `key` attribute into self.dict() view, because Deta uses `key` rather than `id` as 
+        the "primary key" in their databases.
+        """
+        return {**super().dict(*args, **kwargs), "key": str(self.id)}
+    
+    async def save(self):
+        async with async_db_client(self.db_name) as db:
+            self.version += 1
+            await db.put(self.json())
+            
+    async def update(self, *args, **kwargs):
+        async with async_db_client(self.db_name) as db:
+            new_version = self.version + 1
+            new_props = {**self.dict(), **kwargs, "version": new_version}
+            new_instance = self.__class__(**new_props)
+            await db.put(new_instance.json())
+            
+    async def delete(self):
+        async with async_db_client(self.db_name) as db:
+            await db.delete(str(self.id))
+            
+
