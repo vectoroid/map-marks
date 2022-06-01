75530ae298f234bf6d524d1a75a91101c8d1f47c
Tom Malone <tmalone+git@gmail.com>
2022-06-01 09:23:38 -0400

    Removed superfluous type definition for a primary-key-ish field 'id'

--- mapmarks/api/types.py
+++ mapmarks/api/types.py
@@ -5,5 +5,4 @@ import uuid
 GeolocationCategory = typing.Literal["Reefer", "Tobacco"]
 
 # GeoJSON Types
-GeolocationId = typing.NewType('GeolocationId', uuid.UUID)
 GeojsonType = typing.Literal["Point", "Feature"] # not all GeoJSON types are used, presently
