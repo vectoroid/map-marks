9a07bde20e7d6ea716fe15a5f244a737af2b92a6
Tom Malone <tmalone+git@gmail.com>
2022-06-01 14:22:13 -0400

    Fixed imort statements

--- mapmarks/api/__init__.py
+++ mapmarks/api/__init__.py
@@ -5,7 +5,12 @@ MapMarkr app - save your place in life
 __version__ = "0.0.1"
 
 # import app modules
-from .config import AppSettings
-# from .schema import *
-from .types import Position
-from .types import Props
+from mapmarks.api.config import AppSettings
+from mapmarks.api.models.geojson import Position
+from mapmarks.api.models.geojson import Point
+from mapmarks.api.models.geojson import FeatureInRequest
+from mapmarks.api.models.geojson import FeatureInDb
+from mapmarks.api.models.geojson import FeatureCollectionInRequest
+from mapmarks.api.models.geojson import FeatureCollectionInDb
+from mapmarks.api.models.geojson import PropsInRequest
+from mapmarks.api.models.geojson import PropsInDb
