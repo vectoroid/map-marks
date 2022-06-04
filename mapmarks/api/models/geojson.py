e8f7976fa12ef2d9b8a773c51925bae36d11f59d
Tom Malone <tmalone+git@gmail.com>
2022-06-04 04:48:27 -0400

    Removed TimestampMixin class - was only needed in one class, and wasn't able to save much time or redundancy.

--- mapmarks/api/models/geojson.py
+++ mapmarks/api/models/geojson.py
@@ -3,11 +3,16 @@ GeoJSON models
 
 -  defined iac with the [GeoJSON specification: RFC 7946](https://tools.ietf.org/html/rfc7946)
 """
+from datetime import datetime
+
 from pydantic import BaseModel
-from pydantic import confloat
 from pydantic import Field
+# @todo Remove this import statement -- no longer needed
 from pydantic import validator
-from typing import List, Optional, Union
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
 from uuid import UUID, uuid4
 
 from mapmarks.api.config import AppSettings
@@ -15,31 +20,11 @@ from mapmarks.api.models.base import DetaBase, async_db_client
 from mapmarks.api.interfaces import TimestampMixin
 from mapmarks.api.types import GeojsonType
 from mapmarks.api.types import GeolocationCategory
+from mapmarks.api.types import Position
 
 # init 
 settings = AppSettings()
-
-
-# GeoJSON Position element
-class Position(BaseModel):
-    lon: confloat(ge=-180, le=180) = Field(..., alias="Longitude")
-    lat: confloat(ge=-90, le=90) = Field(..., alias="Latitude")
     
-    def __call__(self, lon:float, lat:float) -> List:
-        self.lon = lon
-        self.lat = lat
-        
-        return [self.lon, self.lat]
-    
-    def __repr__(self):
-        return str(tuple(self.lon, self.lat))
-    
-    def __str__(self):
-        self.__repr__()
-        
-    def __format__(self):
-        return f"[lon={self.lon}, lat={self.lat}]"
-
 
 class Point(BaseModel):
     """
@@ -48,32 +33,18 @@ class Point(BaseModel):
     -  NOTE: no `id` or `key` attribute needed -- this model will be nested within the 
              Feature* classes.
     """
-    type: GeojsonType = Field(GeojsonType.POINT.value, const=True)
+    type: GeojsonType = Field(GeojsonType.POINT, const=True)
     coordinates: Position
     
     class Config:
         use_enum_values: bool = True # Use Enum.ITEM.value, rather than the raw Enum
 
-
-# Since the GeoJSON spec--[RFC7946](https://tools.ietf.org/html/rfc7946)--disallows arbitrary attributes/properties assigned to objects defined by the spec,
-# it permits the three objects: `Feature`, `FeatureCollection`, and `GeometryCollection` to have an attributes, `properties`.
-# *  the `properties` attribute is explicitly designed as a place to define any such arbitrary attributes. 
-# *  we include, for instance, timestamps in `properties`, as well as a Feature's:
-#    **  `name`
-#    **  `notes`
-#    **  `category`
-class PropsInRequest(BaseModel):
-    """
-    """
-    name: str
-    note: Optional[str]
-    category: GeolocationCategory
     
-class PropsInDb(PropsInRequest, TimestampMixin):
+class Props(BaseModel):
     """
-    class: PropsInDb
+    class: Props
     -  NOTE: this class needs no `id` or `key` attribute, because it is not saved to 
-             Deta Base directly--it is a "sub-model" of the Feature* classes.
+             Deta Base directly
     
     -  After receiving input, yet before saving this data to Deta Base,
        we need to add the following fields & corresponding values:
@@ -82,22 +53,14 @@ class PropsInDb(PropsInRequest, TimestampMixin):
     -  this endeavor is looked after by the TimestampMixin() class.
     """
     # all timestamping functionality is defined in the TimestampMixin() class
-    pass
-    
-
-class FeatureInRequest(DetaBase):
-    """
-    """
-    type: GeojsonType = Field(GeojsonType.FEATURE.value, const=True)
-    geometry: Point
-    properties: PropsInRequest
-    
-    class Config:
-        title: str = "Geolocation"
-        use_enum_values: bool = True # Use Enum.ITEM.value, rather than the raw Enum
+    name: str
+    note: Optional[str]
+    category: GeolocationCategory
+    created: datetime = Field(default_factory=datetime.utcnow)
+    updated: datetime = Field(default_factory=datetime.utcnow)
 
         
-class FeatureInDb(DetaBase):
+class Feature(DetaBase):
     """Represents a [GeoJSON] Feature object which has been saved to Deta Base.
     
     Attributes
@@ -111,44 +74,33 @@ class FeatureInDb(DetaBase):
 
         However, for this app, I anticipate needing only the Point geometric object.
         
-    properties : PropsInDb
+    properties : Props
         These are the user-defined, application-specific properties (i.e. metadata) that shall be attached 
         to a particular instance of this class.
     """
-    type: GeojsonType = Field(GeojsonType.FEATURE.name, const=True)
+    type: GeojsonType = Field(GeojsonType.FEATURE, const=True)
     geometry: Point
-    properties: PropsInDb
+    properties: Props
     
     class Config:
         title: str = "Geolocation"
         use_enum_values: bool = True # Use Enum.ITEM.value, rather than the raw Enum
         
-    # Is this custom validator even necessary? I mean, shouldn't Pydantic catch a bad `type` input,
-    # based on the attribute type and Field definitions?
-    #
-    # @validator("type")
-    # def type_must_be_valid(cls, v):
-    #     valid_type = GeojsonType.FEATURE.value
-        
-    #     if v is not valid_type:
-    #         error_msg = f"A GeoJSON '{valid_type}' object must have 'type'='{valid_type}'; you provided: 'type'='{v}'."
-    #         raise TypeError( error_msg )
-    #     return v
     
 
-class FeatureCollectionInRequest(DetaBase):
+class FeatureCollection(DetaBase):
     """
-    A class to represent a collection of FeatureInRequest instances
+    A class to represent a collection of Feature instances
     
     Attributes
     ----------
     features : List 
-        A list whose items are: FeatureInRequest() instances
+        A list whose items are: Feature() instances
     
     @todo: overload the `update()` and `delete()` instance methods. Anything else need to be refactored?
     """
-    type: str = Field(GeojsonType.FEATURE_COLLECTION.value, const=True)
-    features: Union[List[FeatureInRequest], List[None]]
+    type: str = Field(GeojsonType.FEATURE_COLLECTION, const=True)
+    features: Union[List[Feature], List[None]]
     
     class Config:
         title: str = "GeolocationCollection"
@@ -159,7 +111,7 @@ class FeatureCollectionInRequest(DetaBase):
         
         @note: it is necessary to overload the `save()` method in this class, because 
                the `super().save()` method is designed to deal with a single instance 
-               per call, whereas all FeatureCollection* classes need to iterate through
+               per call, whereas the FeatureCollection class needs to iterate through
                the `Feature()` instances in their respective `features` list & save each
                one.
         """
@@ -169,13 +121,7 @@ class FeatureCollectionInRequest(DetaBase):
             for instance in self.features:
                 instance.version += 1
                 saved_data = await db.put(instance.json())
-                saved_items.append(self.__class__(**saved_data))
+                saved_items.append(Feature(**saved_data))
                 
             return saved_items
     
-class FeatureCollectionInDb(FeatureCollectionInRequest):
-    """
-    class FeatureCollectionInDb
-    -  items in `features` list are: FeatureInDb() instances
-    """
-    features: Union[List[FeatureInDb], List[None]]
