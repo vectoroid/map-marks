035a221f66072f055a7feb2f4bef3ca980780caf
Tom Malone <tmalone+git@gmail.com>
2022-06-05 14:52:50 -0400

    Fixed broken import statement

--- mapmarks/api/__init__.py
+++ mapmarks/api/__init__.py
@@ -6,7 +6,7 @@ __version__ = "0.0.1"
 
 # import app modules
 from mapmarks.api.config import AppSettings
-from mapmarks.api.types import Position
+from mapmarks.api.types import Lon, Lat
 from mapmarks.api.models.geojson import Point
 from mapmarks.api.models.geojson import Props
 from mapmarks.api.models.geojson import Feature
