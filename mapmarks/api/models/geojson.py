ece223e5e0844a90eca97a19576976d13ccadc8f
Tom Malone <tmalone+git@gmail.com>
2022-06-17 17:08:13 -0400

    Refactored Pydantic models/schema

--- mapmarks/api/models/geojson.py
+++ mapmarks/api/models/geojson.py
@@ -3,18 +3,18 @@ GeoJSON models
 
 -  defined iac with the [GeoJSON specification: RFC 7946](https://tools.ietf.org/html/rfc7946)
 """
-from code import interact
+
+
 from datetime import datetime
 from unicodedata import category
 
 from pydantic import BaseModel
 from pydantic import Field
-# @todo Remove this import statement -- no longer needed
-from pydantic import validator
 from pydantic import root_validator
+from pydantic import validator
 from typing import List
 from typing import Optional
-from typing import Tuple
+from typing import NamedTuple
 from typing import Union
 from uuid import UUID, uuid4
 
@@ -22,77 +22,69 @@ from mapmarks.api.config import AppSettings
 from mapmarks.api.models.base import DetaBase, async_db_client
 from mapmarks.api.types import GeojsonType
 from mapmarks.api.types import GeolocationCategory
-from mapmarks.api.types import Position
+from mapmarks.api.types import Lon, Lat
 
 # init 
 settings = AppSettings()
 
+class Position(NamedTuple):
+    lon: Lon
+    lat: Lat
+
+
 class Point(BaseModel):
     type: GeojsonType = Field(GeojsonType.POINT, const=True)
-    coordinates: list[float]
+    coordinates: Position
     
-    @validator("coordinates")
-    def check_coordinates_length(cls, v):
-        # validate length
-        assert len(v) == 2, "The `coordinates` attribute should be a list of length 2, e.g. [longitude, latitude]"
+    @validator("coordinates", allow_reuse=True)
+    def check_coordinates(cls, v):
+        if len(v) is not 2:
+            raise ValueError("The coordinates attribute must have a magniture, or length, of 2 items: [longitude, latitiude]")
+        
+        lon = v[0]
+        lat = v[1]
+        
+        if lon <= -180.0 or lon >= 180.0:
+            raise ValueError("Longitude must be no less than -180.00 and no more than 180.0.")
+        
+        if lat <= -90.0 or lat >= 90.0:
+            raise ValueError("Latitude must be no less than -90.0 and no more than 90.0")
+        
         return v
-        
-    @validator("coordinates")
-    def check_longitude_within_bounds(cls, v):
-        # we should have: (-180.0 <= longitude <= 180.0)
-        longitude = v[0]
-        
-        if longitude <= -180.0 or longitude >= 180.0:
-            raise ValueError("Longitude must be within the range of: [-180.0, 180.0]")
-        
-        return v
+
     
-    @validator("coordinates")
-    def check_latitude_within_bounds(cls, v):
-        # we should have: (-90.0 <= latitude <= 90.0)
-        latitude = v[1]
-        
-        if latitude <= -90.0 or latitude >= 90.0:
-            raise ValueError("Latitude must be within the range of: [-90.0, 90.0]")
-        
-        return v
 
 class Props(BaseModel):
     """Represents the `properties` key in the GeoJSON spec. 
     
     A GeoJSON object can have custom/arbitrary attributes, not defined in the spec.
     """
-    title: str
+    title: str  # this is the 'title' of the record in Deta Base
     note: Optional[str]
     category: GeolocationCategory
-    version: int
-    created: datetime = Field(default_factory=datetime.now, const=True) # should not change
+    created: datetime = Field(default_factory=datetime.now) # should not change
     updated: datetime = Field(default_factory=datetime.now)
+    version: Optional[int] = Field(1, ge=1)
+    
+    class Config:
+        underscore_attrs_are_private: bool = True
+    
+    def increment_version(self) -> int:
+        """Props.increment_version -- adds 1 to the value of Props.version
+           -  needed when saving and updating instances of this model
+        """
+        self.version += 1
+        return self.version
 
         
 class Feature(DetaBase):
     """classFeature -- represents a GeoJSON Feature object (i.e. a place of interest on a map"""   
     type: GeojsonType = Field(GeojsonType.FEATURE, const=True)
-    geometry: dict[type:GeojsonType.POINT, coordinates:list[float]]
+    geometry: Point
     properties: Props
     class Config:
-        title: str = "Geolocation"
+        title: str = "Geolocation" # This is the title for this resource, as represented by Open API
         use_enum_values: bool = True # Use Enum.ITEM.value, rather than the raw Enum
-        
-    @validator("geometry")
-    def check_coordinates(cls, v):
-        assert len(v.coordinates) == 2, "Coordinates should be a list containing just two items: [longtitude, latitude]"
-        
-        longitude = v.coordinates[0]
-        latitude = v.coordinates[1]
-        
-        if longitude <= -180.0 or longitude >= 180.0:
-            raise ValueError("The first item in the `Feature.geometry.coordinates` list should be the Longitude value, between -180.0 and 180.0")
-
-        if latitude <= -90.0 or latitude >= 90.0:
-            raise ValueError("The second item in the `Feature.geometry.coordinates` list should be the Latitude value, between -90.0 and 90.0")
-        
-        return v
     
 
 
