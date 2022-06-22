ca45a3ff877d1b43ee669f5b9527b48928a4df42
Tom Malone <tmalone+git@gmail.com>
2022-06-22 14:05:08 -0400

    Fixed issue with db_name being assigned the wrong (and a strange) value. Added root_path in order to customize API URLs.

--- mapmarks/api/config.py
+++ mapmarks/api/config.py
@@ -13,16 +13,17 @@ class AppSettings(BaseSettings):
     """
     App-wide configuration/settings
     """
+    api_prefix: str = "/api/v1"
     title: str = "MapMarkr"
     description: str = "Save your favorite places"
     version: str = "0.0.1"
     debug_mode: bool = True
-    db_name: str = Field(env="DETA_DB_NAME")
+    db_name: str
     db_fetch_limit: int = Field(25, const=True)
         
     class Config:
-        case_sensitive: bool = True
         env_file: str = "../../.env"
+        env_prefix: str = "DETA_"
         
         
 settings = AppSettings()
