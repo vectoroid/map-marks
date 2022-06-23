56771663e1a97856ed5dfc2cc281f9562398260a
Tom Malone <tmalone+git@gmail.com>
2022-06-23 05:18:56 -0400

    Separated AppSettings attributes (i.e. the app's config directives) into sections delimited by comments identifying each section.

--- mapmarks/api/config.py
+++ mapmarks/api/config.py
@@ -14,14 +14,18 @@ class AppSettings(BaseSettings):
     """
     App-wide configuration/settings
     """
+    # FastAPI config options
     root_path: str = "/api/v1"
     title: str = "MapMarkr"
     description: str = "Save your favorite places"
     version: str = "0.0.1"
     debug_mode: bool = True
+    # DB config options
     db_name: str
     db_fetch_limit: int = Field(25, const=True)
-        
+    # Logging config options
+    
+    # Meta config options
     class Config:
         env_file: str = "../../.env"
         env_prefix: str = "DETA_"
