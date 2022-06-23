9a642595f3137e3c40bd9c4c5815a4f92f56a839
Tom Malone <tmalone+git@gmail.com>
2022-06-23 06:57:26 -0400

    Fixed broken import statement

--- mapmarks/__init__.py
+++ mapmarks/__init__.py
@@ -6,7 +6,10 @@ __version__ = "0.0.1"
 
 # import app modules
 from deta import Deta
-from mapmarks.api.config import settings
+from mapmarks.api.config import get_app_config
+
+# Get app config settings
+settings = get_app_config()
 
 # initialize Deta Base
 deta = Deta()
